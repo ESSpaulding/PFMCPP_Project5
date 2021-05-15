8832a6c828a33e8c9f2bd0d3e46075502e05c6e8
ESSpaulding <>
2021-05-15 05:14:31 +0000

    Finished wrapper classes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 /*
  Project 5: Part 3 / 4
- video: Chapter 3 Part 4: 
-
+ video: Chapter 3 Part 4:
 Create a branch named Part3
-
  the 'new' keyword
-
  1) add #include "LeakedObjectDetector.h" to main
  
  2) Add 'JUCE_LEAK_DETECTOR(OwnerClass)' at the end of your UDTs.
@@ -14,10 +11,10 @@ Create a branch named Part3
  
  4) write wrapper classes for each type similar to how it was shown in the video
  
- 5) update main() 
+ 5) update main()
       replace your objects with your wrapper classes, which have your UDTs as pointer member variables.
       
-    This means if you had something like the following in your main() previously: 
+    This means if you had something like the following in your main() previously:
 */
 #if false
  Axe axe;
@@ -35,18 +32,13 @@ std::cout << "axe sharpness: " << axWrapper.axPtr->sharpness << "\n";
 /*
 notice that the object name has changed from 'axe' to 'axWrapper'
 You don't have to do this, you can keep your current object name and just change its type to your Wrapper class
-
  8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  
  see here for an example: https://repl.it/@matkatmusic/ch3p04example
-
  you can safely ignore any warnings about exit-time-destructors.
- if you would like to suppress them, add -Wno-exit-time-destructors to the .replit file 
+ if you would like to suppress them, add -Wno-exit-time-destructors to the .replit file
    with the other warning suppression flags
  */
-
-
-
 
 #include <iostream>
 #include <vector> //not using yet
@@ -54,7 +46,7 @@ You don't have to do this, you can keep your current object name and just change
 #include <map>  //not using yet
 #include <ctime>
 #include <unistd.h>
-#include "LeakedObjectDetector.h"
+#include "LeakedObjectDetector.h"   //1)
 
 /*
  copied UDT 1:
@@ -62,19 +54,19 @@ You don't have to do this, you can keep your current object name and just change
 
 struct Oscilloscope
 {
-    float horizontalInput; //initialized in-class
+    float horizontalInput;
     float verticalInput;
     int horizontalGain;
     int verticalGain;
     int horizontalSweepControl;
 
-    Oscilloscope(); //constructor
+    Oscilloscope();
     ~Oscilloscope();
     void graphVoltageOverTime(float yInput, float sweep);
     void graphWaveformDifference (int channelA, int channelB);
     float measureVoltage (float channelA, float channelB);
     float measurement ( float a, float b );
-
+    
     JUCE_LEAK_DETECTOR(Oscilloscope)
 };
 
@@ -82,8 +74,8 @@ Oscilloscope::Oscilloscope() : horizontalInput(0.0f),
 verticalInput(0.0f),
 horizontalGain(1),
 verticalGain(1),
-horizontalSweepControl(1) 
-{ 
+horizontalSweepControl(1)
+{
 
 }
 
@@ -116,14 +108,29 @@ float Oscilloscope::measurement ( float a, float b )
         for (int j = 0; j < b; ++j)
         {
             std::cout << j ;
-            horizontalInput += 1;  // modifying member variable each time through nested loop
+            horizontalInput += 1;
         }
-        verticalInput += 1;        // modifying member variable each time through loop
+        verticalInput += 1;
     }
     
     std::cout << std::endl;
     return a + b;
 }
+//oscilloscope wrapper
+struct OscilloscopeWrapper
+{
+    OscilloscopeWrapper( Oscilloscope* ptr ) : pointerToOscilloscope( ptr )
+    {
+        std::cout << "Oscilloscope Ctor\n";
+    }
+    ~OscilloscopeWrapper()
+    {
+        std::cout << "deleting ptr to Oscilloscope object\n";
+        delete pointerToOscilloscope;
+    }
+    
+    Oscilloscope* pointerToOscilloscope = nullptr;
+};
 
 /*
  copied UDT 2:
@@ -155,6 +162,8 @@ struct CellPhone
         void getFingerPosition(int X, int Y);
         void quickSwipe(bool swipeUp, bool swipeDown);
         void backLightTimer(float timer);
+        
+        JUCE_LEAK_DETECTOR(TouchScreen)
     };
 
     void makeCall (int phoneNumber, std::string personYouAreCalling);
@@ -164,16 +173,18 @@ struct CellPhone
     void cellPhoneFunction();
 
     TouchScreen touchScreen;
+    
+    JUCE_LEAK_DETECTOR(CellPhone)
 };
 
-CellPhone::CellPhone() : screenSize(3), gigabytesOfRAM(128), hasSDCardPort(false), phoneHasHeadphoneJack(true) 
-{ 
+CellPhone::CellPhone() : screenSize(3), gigabytesOfRAM(128), hasSDCardPort(false), phoneHasHeadphoneJack(true)
+{
     
 }
 
-CellPhone::~CellPhone() 
-{ 
-    std::cout << "CellPhone object destructed\n"; 
+CellPhone::~CellPhone()
+{
+    std::cout << "CellPhone object destructed\n";
 }
 
 void CellPhone::makeCall (int phoneNumber, std::string personYouAreCalling)
@@ -209,15 +220,15 @@ void CellPhone::cellPhoneFunction()
     std::cout << "Screen Size: " << this->screenSize << std::endl;
 }
 
-CellPhone::TouchScreen::TouchScreen() :  screenHeight(4.5f), screenWidth(3.0f), x(0), y(0), numberOfGestures(3), backLightOn(true) 
-{ 
-    std::cout << "TS Ctr \n"; 
+CellPhone::TouchScreen::TouchScreen() :  screenHeight(4.5f), screenWidth(3.0f), x(0), y(0), numberOfGestures(3), backLightOn(true)
+{
+    std::cout << "TS Ctr \n";
 }
 
-CellPhone::TouchScreen::~TouchScreen() 
-{ 
-    std::cout << "TS Dtr\n"; 
-}  
+CellPhone::TouchScreen::~TouchScreen()
+{
+    std::cout << "TS Dtr\n";
+}
 
 void CellPhone::TouchScreen::fingerPrintVerification(bool ownersFinger, bool usersFinger)
 {
@@ -235,7 +246,7 @@ void CellPhone::TouchScreen::quickSwipe(bool swipeUp, bool swipeDown)
 }
 
 void CellPhone::TouchScreen::backLightTimer(float timer)
-{   
+{
     float currentTime = 1.1f;
 
     while (backLightOn)
@@ -248,10 +259,37 @@ void CellPhone::TouchScreen::backLightTimer(float timer)
         }
 
         std::cout << "Screen OFF\n";
-        backLightOn = false;        
+        backLightOn = false;
     }
     std::cout << "Goodbye" << std::endl;
 }
+//cellphone wrapper
+struct CellPhoneWrapper
+{
+    CellPhoneWrapper( CellPhone* ptr ) : pointerToCellPhone( ptr )
+    {
+        std::cout << "CellPhone ctor\n";
+    }
+    ~CellPhoneWrapper()
+    {
+        std::cout << "deleting ptr to cellPhone object\n";
+        delete pointerToCellPhone;
+        pointerToCellPhone = nullptr;
+    }
+    CellPhone* pointerToCellPhone = nullptr;
+};
+
+struct TouchScreenWrapper
+{
+    TouchScreenWrapper( CellPhone::TouchScreen* ptr ) : pointerToTouchScreen( ptr ) {}
+    ~TouchScreenWrapper()
+    {
+        delete pointerToTouchScreen;
+        pointerToTouchScreen = nullptr;
+    }
+    
+    CellPhone::TouchScreen* pointerToTouchScreen = nullptr;
+};
 
 /*
  copied UDT 3:
@@ -270,6 +308,8 @@ struct ParametricEq
     void rumbleFilter (float rumbleFrequency, float filterCut);
     void killFeedback (float feedbackFrequency, float gainReduction);
     void centerBandFunction();
+    
+    JUCE_LEAK_DETECTOR(ParametricEq)
 };
 
 ParametricEq::ParametricEq() :
@@ -278,18 +318,18 @@ highShelfFrequency(1800.0f),
 centerBandFrequency(0.5f),
 centerBandGain(0.5f),
 centerBandSlope(0.7f)
-{ 
+{
     std::cout << "ParametricEQ object constructed " << std::endl;
 }
 
 ParametricEq::~ParametricEq() //2)
-{ 
-    std::cout << "ParametricEQ object destructed\n"; 
-}   
+{
+    std::cout << "ParametricEQ object destructed\n";
+}
 
 void ParametricEq::vocalDeEss (float sibilanceAmount, float sibilanceSuppression)
 {
-    centerBandGain = centerBandGain - sibilanceAmount;   //is this an example of "implied this" ?
+    centerBandGain = centerBandGain - sibilanceAmount;
     sibilanceSuppression = 0.f;
     std::cout << "Vocal DeEss engaged.  Fc: " << this->centerBandFrequency << " gain reduced to: " << this->centerBandGain << std::endl;
 }
@@ -314,6 +354,18 @@ void ParametricEq::centerBandFunction()
     std::cout << "Fc: " << this->centerBandFrequency << "  Gain: " << this->centerBandGain << "  Slope: " << this->centerBandSlope << std::endl; // 2b)
 }
 
+struct ParametricEqWrapper
+{
+    ParametricEqWrapper( ParametricEq* ptr ) : pointerToParametricEq( ptr ) {}
+    ~ParametricEqWrapper()
+    {
+        delete pointerToParametricEq;
+        pointerToParametricEq = nullptr;
+    }
+    
+    ParametricEq* pointerToParametricEq = nullptr;
+};
+
 /*
  new UDT 4:
  */
@@ -329,16 +381,19 @@ struct MeasurementRack
     
     float overallGain();
     float power();
-    void memberGainFunction();  //2a
-    void memberPowerFunction();  //2a  member function that accesses member variable of owning instance of object
+    void memberGainFunction();
+    void memberPowerFunction();
+    
+    JUCE_LEAK_DETECTOR(MeasurementRack)
 };
 
-MeasurementRack::MeasurementRack()   //5)  constructor that does something and calls member function
+MeasurementRack::MeasurementRack()
 {
     std::cout << "MeasurementRack constructed\nHorizontal Gain: " << inputOscope.horizontalGain << std::endl;
     
 }
-MeasurementRack::~MeasurementRack()  //5) destructor that does something on shutdown
+
+MeasurementRack::~MeasurementRack()
 {
     inputOscope.horizontalGain = 0;
     inputOscope.verticalGain = 0;
@@ -353,25 +408,38 @@ void rackFunction()  //free function called from main
     measurementRack.supplyScope.measureVoltage(100, 121);
 }
 
-float MeasurementRack::overallGain()  // 4) member function #1 of  1st new UDT
+float MeasurementRack::overallGain()
 {
-    return inputOscope.measureVoltage(1.5f, 2.21f) + outputOscope.measureVoltage(6.3f, 1.1f);  //measureVoltage should return a float too*******
+    return inputOscope.measureVoltage(1.5f, 2.21f) + outputOscope.measureVoltage(6.3f, 1.1f);
 }
 
-float MeasurementRack::power()       // 4) member function #2 of  1st new UDT
+float MeasurementRack::power()
 {
     return (outputOscope.measurement(2.1f, 3.1f) * outputOscope.measurement(1.02f, 1.11f)) / supplyScope.measurement(1.01f, 1.11f);
 }
 
 void MeasurementRack::memberGainFunction()  //2a)
 {
-    std::cout << "overall gain is: " << this->overallGain() << std::endl;  // 2b)
+    std::cout << "overall gain is: " << this->overallGain() << std::endl;
 }
 
-void MeasurementRack::memberPowerFunction()  //2a)
+void MeasurementRack::memberPowerFunction()
 {
-    std::cout << "overall power is: " << this->power() << std::endl;   // 2b) measurementRack.power()no go, because that instance exists outside
+    std::cout << "overall power is: " << this->power() << std::endl;
 }
+
+struct MeasurementRackWrapper
+{
+    MeasurementRackWrapper( MeasurementRack* ptr ) : pointerToMeasurementRack( ptr ) {}
+    ~MeasurementRackWrapper()
+    {
+        delete pointerToMeasurementRack;
+        pointerToMeasurementRack = nullptr;
+    }
+    
+    MeasurementRack* pointerToMeasurementRack = nullptr;
+};
+
 /*
  new UDT 5:
  3) using only the types copied above as member functions. 4) with 2 member functions
@@ -388,16 +456,18 @@ struct FourBandPEQ
     
     void dimeBagPEQ();
     void speakerCompensation();
+    
+    JUCE_LEAK_DETECTOR(FourBandPEQ)
 };
 
-FourBandPEQ::FourBandPEQ() 
-{ 
-    std::cout <<"FourBandPEQ object constructed\n"; 
+FourBandPEQ::FourBandPEQ()
+{
+    std::cout <<"FourBandPEQ object constructed\n";
 }
 
-FourBandPEQ::~FourBandPEQ() 
-{ 
-    std::cout << "FourBandPEQ object destroyed\n"; 
+FourBandPEQ::~FourBandPEQ()
+{
+    std::cout << "FourBandPEQ object destroyed\n";
 }
 
 void FourBandPEQ::dimeBagPEQ()
@@ -421,60 +491,76 @@ void FourBandPEQ::speakerCompensation()
     std::cout << "Your budget speakers have been optimized\n";
 }
 
+struct FourBandPEQWrapper
+{
+    FourBandPEQWrapper( FourBandPEQ* ptr ) : pointerToFourBandPEQ( ptr ) { }
+    ~FourBandPEQWrapper()
+    {
+        delete pointerToFourBandPEQ;
+        pointerToFourBandPEQ = nullptr;
+    }
+    
+    FourBandPEQ* pointerToFourBandPEQ = nullptr;
+};
+
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
-
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
  If you didn't already:
     Make a pull request after you make your first commit
     pin the pull request link and this repl.it link to our DM thread in a single message.
-
  send me a DM to review your pull request when the project is ready for review.
-
  Wait for my code review.
  */
 
 #include <iostream>
 int main()
 {
-    Oscilloscope oScope;  //3) instatiation of UDT
-    oScope.measureVoltage(2.01f, 4.123f);
-    oScope.graphWaveformDifference(32, 16);
-    oScope.measurement(5.123f, 6.321f);
+    Oscilloscope* oScope = new Oscilloscope;
+    OscilloscopeWrapper oscilloscopeWrapper( oScope );
+    oScope->measureVoltage(2.01f, 4.123f);
+    oScope->graphWaveformDifference(32, 16);
+    oScope->measurement(5.123f, 6.321f);
 
     rackFunction();  //free function that tests MeasurementRack
-    MeasurementRack measurementRack;
-    std::cout << "overall gain is: " << measurementRack.overallGain() << std::endl;  //1)  main std::cout
-    measurementRack.memberGainFunction();  //2c) print the same as above, but from inside the member function
-    std::cout << "overall power is: " << measurementRack.power() << std::endl;  //1) main std::cout
-    measurementRack.memberPowerFunction();  //2c)
+    MeasurementRack* measurementRack = new MeasurementRack;
+    MeasurementRackWrapper measurementRackWrapper( measurementRack );
+    std::cout << "overall gain is: " << measurementRack->overallGain() << std::endl;
+    measurementRack->memberGainFunction();
+    std::cout << "overall power is: " << measurementRack->power() << std::endl;
+    measurementRack->memberPowerFunction();
     
-    CellPhone myPhone;
-    CellPhone yourPhone;
-    myPhone.makeCall(231345632, "Karen");
-    myPhone.dropPhone(2);
-    std::cout << "Screen Size: " << yourPhone.screenSize << std::endl;  //1)
-    yourPhone.cellPhoneFunction();                 //2c)  screen size will not be the same because this function changes the objects member variable value
+    CellPhone* myPhone = new CellPhone();
+    CellPhoneWrapper myPhoneWrapper( myPhone );
+    CellPhone* yourPhone = new CellPhone();
+    CellPhoneWrapper yourPhoneWrapper( yourPhone );
+    myPhone->makeCall(231345632, "Karen");
+    myPhone->dropPhone(2);
+    std::cout << "Screen Size: " << yourPhone->screenSize << std::endl;
+    yourPhone->cellPhoneFunction();
     
-    CellPhone::TouchScreen myTouchscreen;
-    myTouchscreen.fingerPrintVerification(true, false);
-    yourPhone.makeCall(8675301, "Daryl");
+    CellPhone::TouchScreen* myTouchscreen = new CellPhone::TouchScreen();
+    TouchScreenWrapper myTouchScreenWrapper( myTouchscreen );
+    myTouchscreen->fingerPrintVerification(true, false);
+    yourPhone->makeCall(8675301, "Daryl");
     CellPhone::TouchScreen yourTouchscreen;
     yourTouchscreen.fingerPrintVerification(true, true);
-    myTouchscreen.backLightTimer(2);
+    myTouchscreen->backLightTimer(2);
 
-    ParametricEq pEQ;
-    pEQ.rumbleFilter(30.0f, 120.0f);
-    pEQ.killFeedback(220, .15f);
-    std::cout << "Vocal DeEss engaged.  Fc: " << pEQ.centerBandFrequency << " gain reduced to: " << pEQ.centerBandGain << std::endl; //1)
-    pEQ.vocalDeEss(21.f, 30.f);  //2c) this function leaves a previous "implied this->" that modifies member variables.
-    std::cout << "Fc: " << pEQ.centerBandFrequency << "  Gain: " << pEQ.centerBandGain << "  Slope: " << pEQ.centerBandSlope << std::endl;  //1)
-    pEQ.centerBandFunction();                                                                                                               //2c)
+    ParametricEq* pEQ = new ParametricEq();
+    ParametricEqWrapper parametricEqWrapper( pEQ );
+    pEQ->rumbleFilter(30.0f, 120.0f);
+    pEQ->killFeedback(220, .15f);
+    std::cout << "Vocal DeEss engaged.  Fc: " << pEQ->centerBandFrequency << " gain reduced to: " << pEQ->centerBandGain << std::endl; //1)
+    pEQ->vocalDeEss(21.f, 30.f);
+    std::cout << "Fc: " << pEQ->centerBandFrequency << "  Gain: " << pEQ->centerBandGain << "  Slope: " << pEQ->centerBandSlope << std::endl;
+    pEQ->centerBandFunction();
     
-    FourBandPEQ fbPEQ;
-    fbPEQ.dimeBagPEQ();
-    fbPEQ.speakerCompensation();
+    FourBandPEQ* fbPEQ = new FourBandPEQ();
+    FourBandPEQWrapper fourBandPEQWrapper( fbPEQ );
+    fbPEQ->dimeBagPEQ();
+    fbPEQ->speakerCompensation();
 
     std::cout << "good to go!\n" << std::endl;
 }
